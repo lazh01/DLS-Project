a6e2fdc87c961331c8dd637e6f323e51cf6ffd1f
Sebastian <seb0465@hotmail.com>
2025-09-29 14:38:58 +0200

    Initial draft service with crud using entity framework

---
+++ DraftService/Data/DraftServiceContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using DraftService.Models;
+
+namespace DraftService.Data
+{
+    public class DraftServiceContext : DbContext
+    {
+        public DraftServiceContext (DbContextOptions<DraftServiceContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<DraftService.Models.Draft> Draft { get; set; } = default!;
+    }
+}
---
+++ DraftService/Program.cs
@@ -0,0 +1,54 @@
+using DraftService.Data;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var connectionString1 = builder.Configuration.GetConnectionString("DraftDatabase");
+var connectionString = "Server=draft-db;Database=Drafts;User Id=sa;Password=SuperSecret7!;Encrypt=false;";
+builder.Services.AddDbContext<DraftServiceContext>(options =>
+{
+    if (!string.IsNullOrWhiteSpace(connectionString))
+    {
+        options.UseSqlServer(connectionString);
+    }
+    else
+    {
+        options.UseInMemoryDatabase("DraftsInMemory");
+    }
+});
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddControllersWithViews();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<DraftServiceContext>();
+
+    if (db.Database.IsSqlServer())
+    {
+        db.Database.Migrate(); // applies migrations to SQL Server only
+    }
+}
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
